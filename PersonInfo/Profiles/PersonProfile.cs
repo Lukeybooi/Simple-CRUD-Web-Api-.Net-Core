a7cdf157fc0b0fa19d7d66c58dfde57163fe5a7c
Luke <Lukeybooi@gmail.com>
2022-01-02 20:01:58 +0200

    Added Profile

---
+++ PersonInfo/Profiles/PersonProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PersonInfo.Dtos;
+using PersonInfo.Models;
+
+namespace PersonInfo.Profiles
+{
+    public class PersonProfile : Profile
+    {
+        public PersonProfile()
+        {
+            CreateMap<PersonCreateDto, Person>();
+        }
+    }
+}
