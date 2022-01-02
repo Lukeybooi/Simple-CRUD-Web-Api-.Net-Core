637bd98dcb50113f75511d651e2aa810b03f93e6
Luke <Lukeybooi@gmail.com>
2022-01-02 20:00:19 +0200

    Added Enums

---
+++ PersonInfo/Enums/Gender.cs
@@ -0,0 +1,8 @@
+﻿namespace PersonInfo.Enums
+{
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2,
+    }
+}
---
+++ PersonInfo/Enums/Race.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonInfo.Enums
+{
+    public enum Race
+    {
+        Black = 1,
+        White = 2,
+        Indian = 3,
+        Coloured = 4,
+        Asian = 5,
+        Other = 6
+    }
+}
